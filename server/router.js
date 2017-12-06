e29f1c063756bbd3dc9748266616795406f1795e
James Taggart <jxt1607@g.rit.edu>
2017-12-06 14:47:40 -0500

    Minor modification to router

--- server/router.js
+++ server/router.js
@@ -21,9 +21,9 @@ const router = (app) => {
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
-  app.post('/changepass', mid.requiresLogin, controllers.Account.changePass);
 
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+  app.post('/changepass', mid.requiresLogin, controllers.Account.changePass);
 
   app.get('/maker', mid.requiresLogin, controllers.Army.armyPage);
   app.post('/maker', mid.requiresLogin, controllers.Army.makeArmy);
